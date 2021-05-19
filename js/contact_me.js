545bea9b0b2e41a0efb3ee2d2c73c0f574f8df21
bayoticstudios <83475782+Bayotics@users.noreply.github.com>
2021-05-20 00:21:15 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,22 +26,12 @@ $(function() {
             message: message
           },
           cache: false,
-          success: function() {
+            error: function() {
             $('#success').html("<div class='alert alert-success'>");
             $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
               .append("</button>");
-            $('#success > .alert-success')
-              .append("<strong> We received Your Message. </strong>");
-            $('#success > .alert-success')
-              .append('</div>');
-            $('#contactForm').trigger("reset");
-          },
-          error: function() {
-            $('#success').html("<div class='alert alert-danger'>");
-            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-            $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", Our Server is currently not responding. Please try again later!"));
-            $('#success > .alert-danger').append('</div>');
+            $('#success > .alert-success').append($("<strong>").text("Thank You" + firstName + ", We received your Message."));
+            $('#success > .alert-success').append('</div>');
             $('#contactForm').trigger("reset");
           },
           complete: function() {
